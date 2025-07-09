1915f05437fd9f360d0d602c67a84fe4ea8105a4
Thien Lanh <truongthienlanh1999@gmail.com>
2025-07-09 23:23:43 +0700

    filter key db in homepage

--- client/src/pages/homepage/Home.js
+++ client/src/pages/homepage/Home.js
@@ -18,7 +18,7 @@ function Home() {
 
     fetchData();
   }, []);
-
+  let renderedNav4 = false
 
   return (
     <Container>
@@ -29,6 +29,7 @@ function Home() {
         </Container>
       ) : (
         <Box >
+
           {data.map((item, index) => {
             switch (item.type) {
               case 'nav_1':
@@ -77,7 +78,7 @@ function Home() {
                     }}
                   >
                     {Array.isArray(item.image) && item.image.length > 0 && (
-                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2,  }}>
+                      <Box sx={{ display: 'flex', flexWrap: 'wrap', gap: 2, }}>
                         {item.image.map((img, idx) => {
                           switch (idx) {
                             case 0:
@@ -104,7 +105,7 @@ function Home() {
                                   }}
                                 />
                               );
-                              case 2:
+                            case 2:
                               return (
                                 <CardMedia
                                   key={idx}
@@ -118,18 +119,53 @@ function Home() {
                               );
                             default:
                               return null;
-                          }
+                          };
                         })}
                       </Box>
                     )}
                   </Box>
                 );
+              case 'nav_4':
+                if (!renderedNav4) {
+                  renderedNav4 = true;
+                  return (
+                    <Box key={'nav4-page'} height="100vh" sx={{ display: 'flex', flexDirection: 'column', gap: 4 }}>
+                      {data
+                        .filter((i) => i.type === 'nav_4')
+                        .map((i, idx) => (
+                          <Box key={idx} sx={{ display: 'flex', alignItems: 'center', gap: 2 }}>
+                            <Box sx={{ width: 14, height: 14, borderRadius: '50%', bgcolor: 'black' }} />
+                            <Box>
+                              <StyleTypography_3>{i.title}</StyleTypography_3>
+                              <StyleTypography_4>{i.title_2}</StyleTypography_4>
+                            </Box>
+                          </Box>
+                        ))}
+                    </Box>
+                  );
+                }
+                return null;
+
+              case 'nav_5':
+                return (
+                    <Box key={index} height={'100vh'} sx={{ mb: 4 }}>
+                      <StyleTypography_3>{item.title}</StyleTypography_3>
+                    </Box>
+                )
 
               default:
                 return null;
             }
           })
           }
+          {/* <Box height={'100vh'}>
+            {data.filter(item => item.type === 'nav_4').map((item, index) => (
+              <Box key={index} sx={{ mb: 4 }}>
+                <StyleTypography_3>{item.title}</StyleTypography_3>
+                <StyleTypography_4>{item.title_2}</StyleTypography_4>
+              </Box>
+            ))}
+          </Box> */}
         </Box>
       )}
     </Container>
